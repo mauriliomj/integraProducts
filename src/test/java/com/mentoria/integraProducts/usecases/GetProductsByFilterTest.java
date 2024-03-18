c3d849a29473ecb6bffc387cd4aa926c137fc38b
Maurílio <mauriliojose249@gmail.com>
2024-03-17 21:58:11 -0300

    Finalizando testes!

--- src/test/java/com/mentoria/integraProducts/usecases/GetProductsByFilterTest.java
+++ src/test/java/com/mentoria/integraProducts/usecases/GetProductsByFilterTest.java
@@ -1,9 +1,9 @@
 package com.mentoria.integraProducts.usecases;
 
 import static org.mockito.Mockito.mock;
-
 import com.mentoria.integraProducts.domains.Product;
 import com.mentoria.integraProducts.gateways.outputs.ProductDataGateway;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +12,6 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 
 @ExtendWith(MockitoExtension.class)
 class GetProductsByFilterTest {
@@ -25,14 +24,17 @@ class GetProductsByFilterTest {
   public void sholdReturnAnProductsPageByFilters(){
     int pageNumber = 0;
     int pageSize = 10;
+    PageRequest pageable = PageRequest.of(pageNumber, pageSize);
     String sellerId = "testId";
     String brand = "testBrand";
     String category = "testCategory";
-    Sort sort = Sort.TypedSort.by(category);
-    Page<Product> productPage = ;
-    getProductsByFilter.execute(brand, category, sellerId, pageSize, pageNumber);
+    Page<Product> productPage = mock(Page.class);
 
-    Mockito.when(productDataGateway.findBySellerIdAndBrandAndCategory(sellerId, brand, category,
-        PageRequest.of(pageNumber, pageSize, sort))).thenReturn(productPage);
+    Mockito.when(productDataGateway.findBySellerIdAndBrandAndCategory(brand, category, sellerId,
+        pageable)).thenReturn(productPage);
+
+    getProductsByFilter.execute(brand, category, sellerId, pageSize, pageNumber);
+    Assertions.assertEquals(productPage, productDataGateway
+        .findBySellerIdAndBrandAndCategory(brand, category, sellerId, pageable));
   }
 }
--- src/test/java/com/mentoria/integraProducts/usecases/UpdateProductTest.java
+++ src/test/java/com/mentoria/integraProducts/usecases/UpdateProductTest.java
@@ -6,25 +6,25 @@ import com.mentoria.integraProducts.gateways.outputs.ProductDataGateway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-
 @ExtendWith(MockitoExtension.class)
 class UpdateProductTest {
-
   @InjectMocks
   private UpdateProduct updateProduct;
-
   @Mock
   private ProductDataGateway productDataGateway;
+  @Captor
+  private ArgumentCaptor<Product> productArgumentCaptor;
 
   @Test
-  public void shouldChangeAPriceBySkuAndSellerId() {
+  public void shouldChangeAProductBySkuAndSellerId() {
     Mockito.when(
             productDataGateway.findBySkuAndSellerId(mockProductUpdated().getSku(),
                 mockProductUpdated().getSellerId()))
@@ -32,11 +32,25 @@ class UpdateProductTest {
 
     updateProduct.execute(mockProductUpdated());
 
-    Mockito.verify(productDataGateway).save(mockProductUpdated());
+    Mockito.verify(productDataGateway).save(productArgumentCaptor.capture());
+    Product productCaptor = productArgumentCaptor.getValue();
+
+    Assertions.assertEquals(mockProduct().getSku(), productCaptor.getSku());
+    Assertions.assertEquals(mockProduct().getSellerId(), productCaptor.getSellerId());
+    Assertions.assertNotEquals(mockProduct().getLastModifiedDate(), productCaptor
+        .getLastModifiedDate());
+    Assertions.assertNotEquals(mockProduct().getProductName(), productCaptor.getProductName());
+    Assertions.assertNotEquals(mockProduct().getBrand(), productCaptor.getBrand());
+    Assertions.assertNotEquals(mockProduct().getCategory(), productCaptor.getCategory());
+    Assertions.assertNotEquals(mockProduct().getSize(), productCaptor.getSize());
+    Assertions.assertNotEquals(mockProduct().getColor(), productCaptor.getColor());
+    Assertions.assertNotEquals(mockProduct().getDescription(), productCaptor.getDescription());
+    Assertions.assertNotEquals(mockProduct().getFlavor(), productCaptor.getFlavor());
+
   }
 
   @Test
-  public void shouldThrowExceptionForNotFindingId() {
+  public void shouldThrowExceptionForNotFoundException() {
     Mockito.when(productDataGateway.findBySkuAndSellerId(mockProductUpdated().getSku(),
             mockProductUpdated().getSellerId()))
         .thenThrow(new NotFoundException("Id não encontrado."));
@@ -46,18 +60,23 @@ class UpdateProductTest {
   }
 
   public Product mockProduct() {
-
     Product mockProduct = new Product();
     mockProduct.setSku("SkuTest");
     mockProduct.setSellerId("IdTest");
     return mockProduct;
-
   }
 
   public Product mockProductUpdated() {
-    Product mockProductUpdated = new Product();
-    mockProductUpdated.setSku("SkuTest");
-    mockProductUpdated.setSellerId("IdTest");
-    return mockProductUpdated;
+    Product mockProduct = new Product();
+    mockProduct.setSku("SkuTest");
+    mockProduct.setSellerId("IdTest");
+    mockProduct.setProductName("name");
+    mockProduct.setDescription("description");
+    mockProduct.setSize("size");
+    mockProduct.setColor("collor");
+    mockProduct.setFlavor("flavor");
+    mockProduct.setBrand("brand");
+    mockProduct.setCategory("category");
+    return mockProduct;
   }
 }
